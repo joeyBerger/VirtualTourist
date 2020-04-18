dd7afe10bd5adbd3f8279cc06b7040bf703a1f37
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2020-04-18 14:04:12 -0700

    before refactoring

---
+++ VirtualTourist/VirtualTourist/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by Joey Berger on 4/16/20.
+//  Copyright © 2020 Joey Berger. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
